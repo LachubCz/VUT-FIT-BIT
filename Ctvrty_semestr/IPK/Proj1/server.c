58a17fd5d54f8da242eaac59ff8785eeab13f325
LachubCz <petr.genius.buchal@gmail.com>
2017-03-19 19:21:21 +0100

    Update server.c

--- Ctvrty_semestr/IPK/Proj1/server.c
+++ Ctvrty_semestr/IPK/Proj1/server.c
@@ -4,12 +4,27 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <netinet/in.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdbool.h>
 
 #define MAX_PATH 1024 //maximalni delka path
 #define BUFFER 2048 //velikost dat nacitanych ze socketu do bufferu
 
 int PORT; 
 char ROOT_FOLDER[MAX_PATH];
+
+typedef struct {
+	char *command;
+	char *path;
+	char *type;
+	char *host;
+	char *date;
+	char *accept;
+	char *accept_en;
+	char *con_type;
+	char *con_len;
+} Header;
 
 int number_from_string(const char *str)
 {
@@ -94,29 +109,166 @@ void arguments(int argc, char const *argv[])
 	return;
 }
 
-void send_state(int state, int socket_fd2)
+char * getHeader (int message, int num_con_len)
 {
-	char message[17];
-	bzero(message, 17);
-
-	switch (state)
+	char *ret_text;
+	if (message == 0)
 	{
-		case 200:
-			strcpy(message, "200 OK");
-			break;
-		case 404:
-			strcpy(message, "404 Not Found");
-			break;
-		case 400:
-			strcpy(message, "400 Bad Request");
-			break;
+		ret_text = "200 OK";
 	}
+	else if (message == 1)
+		{
+			ret_text = "404 Not Found";
+		}
+	else if (message == 2)
+		{
+			ret_text = "400 Bad Request";
+		}
 
+	char str_con_len[16];
+	sprintf(str_con_len, "%d", num_con_len);
+
+	char *http = "HTTP/1.1 ";
+	char *date = "Date: ";
+	char *con_type = "Content-Type: application/json";
+	char *con_len = "Content-Length: ";
+	char *con_en = "Content-Encoding: identity";
+
+	time_t rawtime;
+	struct tm * timeinfo;
+	char act_time[64];
+	time (&rawtime);
+	timeinfo = localtime (&rawtime);
+	strftime (act_time, 64, "%a, %d %b %G %H:%M:%S CET", timeinfo);
+
+	int length = strlen(http) + strlen(ret_text) + strlen(date) + strlen(act_time) + strlen(con_type) + strlen(con_len) + strlen(str_con_len) + strlen(con_en);
+	
+	char *Header = malloc(length * sizeof(char) + 16 * sizeof(char));
+
+	sprintf(Header, "%s%s\n%s%s\n%s\n%s%s\n%s\n", http, ret_text, date, act_time, con_type, con_len, str_con_len, con_en);
+	
+	return Header;
+}
+
+void send_state(int socket_fd2, char *message)
+{
 	if ((write(socket_fd2, message, strlen(message))) < 0)
 	{
 		fprintf(stderr, "Chyba pri zapisovani do socketu.\n");
 		exit(6);
 	}
+}
+
+void DEL(Header *header, int socket_fd2)
+{
+	printf("%s\n", header->command);
+}
+
+void GET(Header *header, int socket_fd2)
+{
+	printf("%s\n", header->command);
+}
+
+void PUT(Header *header, int socket_fd2)
+{
+	printf("%s\n", header->command);
+}
+
+void LST(Header *header, int socket_fd2)
+{
+	char *path = malloc(MAX_PATH*sizeof(char));
+	sprintf(path,"%s%s", ROOT_FOLDER, header->path);
+	//int errCheck = ls(path);
+}
+
+void MKD(Header *header, int socket_fd2)
+{
+	char *path = malloc(MAX_PATH*sizeof(char));
+	sprintf(path,"%s%s", ROOT_FOLDER, header->path);
+	int errCheck = mkdir(path, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+	if (errCheck == -1)
+	{
+		char *header_str = getHeader(1, 0);
+		send_state(socket_fd2, header_str);
+	}
+	else
+	{
+		char *header_str = getHeader(0, 0);
+		send_state(socket_fd2, header_str);
+	}
+	free(path);
+}
+
+void RMD(Header *header, int socket_fd2)
+{
+	char *path = malloc(MAX_PATH*sizeof(char));
+	sprintf(path,"%s%s", ROOT_FOLDER, header->path);
+	int errCheck = rmdir(path);
+	if (errCheck == -1)
+	{
+		char *header_str = getHeader(1, 0);
+		send_state(socket_fd2, header_str);
+	}
+	else
+	{
+		char *header_str = getHeader(0, 0);
+		send_state(socket_fd2, header_str);
+	}
+	free(path);
+}
+
+void cmd_switch(Header *header, int socket_fd2)
+{
+	if (strcmp(header->command, "DEL") == 0)
+	{
+		DEL(header, socket_fd2);
+	}
+	else if (strcmp(header->command, "GET") == 0)
+	{
+		GET(header, socket_fd2);
+	}
+	else if (strcmp(header->command, "PUT") == 0)
+	{
+		PUT(header, socket_fd2);
+	}
+	else if (strcmp(header->command, "LST") == 0)
+	{
+		LST(header, socket_fd2);
+	}
+	else if (strcmp(header->command, "MKD") == 0)
+	{
+		MKD(header, socket_fd2);
+	}
+	else if (strcmp(header->command, "RMD") == 0)
+	{
+		RMD(header, socket_fd2);
+	}
+}
+
+void HeaderParser(char *header_str, int socket_fd2)
+{
+
+	Header *header = malloc(sizeof(Header));
+
+	header->command = strtok(header_str, " ");
+	header->path = strtok(NULL, "?");
+	strtok(NULL, "=");
+	header->type = strtok(NULL, " ");
+	strtok(NULL, ":");
+	header->host = strtok(NULL, "\n");
+	strtok(NULL, ":");
+	header->date = strtok(NULL, "\n");
+	strtok(NULL, ":");
+	header->accept = strtok(NULL, "\n");
+	strtok(NULL, ":");
+	header->accept_en = strtok(NULL, "\n");
+	strtok(NULL, ":");
+	header->con_type = strtok(NULL, "\n");
+	strtok(NULL, ":");
+	header->con_len = strtok(NULL, "\n");
+	strtok(NULL, ":");
+
+	cmd_switch(header, socket_fd2);
 }
 
 int main(int argc, char const *argv[])
@@ -150,24 +302,29 @@ int main(int argc, char const *argv[])
 	}
 
 	listen(socket_fd, 5);
-
 	int cl_addr_size = sizeof(client_addr);
-	socket_fd2 = accept(socket_fd, (struct sockaddr *) &client_addr, &cl_addr_size);
-	if (socket_fd2 < 0)
+
+	while(true)
 	{
-		fprintf(stderr, "Chyba pri prijmani socketu.\n");
-		exit(4);
+		socket_fd2 = accept(socket_fd, (struct sockaddr *) &client_addr, &cl_addr_size);
+		if (socket_fd2 < 0)
+		{
+			fprintf(stderr, "Chyba pri prijmani socketu.\n");
+			exit(4);
+		}
+
+		if ((read(socket_fd2, buffer, BUFFER)) < 0)
+		{
+			fprintf(stderr, "Chyba pri cteni ze socketu.\n");
+			exit(5);
+		}
+
+		//recieve file function
+		
+		HeaderParser(buffer, socket_fd2);
+
+		//send_state(200, socket_fd2);
 	}
-
-	if ((read(socket_fd2, buffer, BUFFER)) < 0)
-	{
-		fprintf(stderr, "Chyba pri cteni ze socketu.\n");
-		exit(5);
-	}
-
-	printf("\n%s\n", buffer);
-
-	send_state(200, socket_fd2);
 
 	return 0;
 }
