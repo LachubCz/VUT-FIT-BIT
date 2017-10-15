61916e390fdb8c2cde8b4bcd20a7e9e25c9e6d1f
LachubCz <petr.genius.buchal@gmail.com>
2017-10-15 18:06:06 +0400

    Lehké změny na screenshot mockupu pro technickou zprávu

--- 5_semestr/ISA/proj1/isashark.c
+++ 5_semestr/ISA/proj1/isashark.c
@@ -1,53 +1,134 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <string.h>
+
+void PrintHelp()
+{
+	printf("This line is every help you will ever get.\n");
+	exit(0);
+}
+
+void ErrorFound(int i)
+{
+	switch(i)
+	{
+		case 0:
+			fprintf (stderr, "Non valid argument -l.\n");
+			break;
+		case 1:
+			fprintf (stderr, "Option -a requires an argument.\n");
+			break;
+		case 2:
+			fprintf (stderr, "Option -s requires an argument.\n");
+			break;
+		case 3:
+			fprintf (stderr, "Option -l requires an argument.\n");
+			break;
+		case 4:
+			fprintf (stderr, "Option -f requires an argument.\n");
+			break;
+		case 5:
+			fprintf (stderr, "Unknown option.\n");
+			break;
+		case 6:
+			fprintf (stderr, "Unknown option character.\n");
+			break;
+		case 7:
+			printf ("Non-option argument.\n");
+			break;
+		case 8:
+			fprintf (stderr, "Non valid argument combination.\n");
+			break;			
+	}
+	exit(1);
+}
+
+int CharToInt (char *str)
+{
+	int length = strlen(str);
+	int number = 0;
+	for (int i=0; i<length; i++)
+	{
+		if (!isdigit(str[i]))
+		{
+			return -1;
+		}
+		if (str[i] == '-')
+		{
+			return -1;
+		}
+		number = number * 10;
+		number = number + (((int) str[i]) - 48);
+	}
+
+	return number;
+}
 
 int main (int argc, char *argv[])
 {
 	bool hflag = false;
-	char *avalue = NULL;
-	char *svalue = NULL;
-	char *lvalue = NULL;
-	char *fvalue = NULL;
-	int index;
 	int c;
-
+	int index;
+	int lvalue = -2;
+	char avalue[256] = "";
+	char svalue[256] = "";
+	char fvalue[256] = "";
+	
 	opterr = 0;
 
-	while ((c = getopt (argc, argv, "abc:")) != -1)
+	while ((c = getopt (argc, argv, "ha:s:l:f:")) != -1)
 	switch (c)
 		{
 			case 'h':
 				hflag = true;
 				break;
 			case 'a':
-				aflag = optarg;
+				strcpy(avalue, optarg);
 				break;
 			case 's':
-				sflag = optarg;
+				strcpy(svalue, optarg);
 				break;
 			case 'l':
-				lflag = optarg; // kontrola formatu cisla
+				lvalue = CharToInt(optarg);
+				if (lvalue < 0)
+					ErrorFound(0);
 				break;
 			case 'f':
-				fvalue = optarg;
+				strcpy(fvalue, optarg);
 				break;
 			case '?':
-				if (optopt == 'c')
-					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+				if (optopt == 'a')
+					ErrorFound(1);
+				else if (optopt == 's')
+					ErrorFound(2);
+				else if (optopt == 'l')
+					ErrorFound(3);
+				else if (optopt == 'f')
+					ErrorFound(4);
 				else if (isprint (optopt))
-					fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+					ErrorFound(5);
 				else
-					fprintf (stderr,
-									 "Unknown option character `\\x%x'.\n",
-									 optopt);
-				return 1;
+					ErrorFound(6);
 			default:
-				abort ();
+				break;
 			}
 
-	printf ("aflag = %d, bflag = %d, cvalue = %s\n", aflag, bflag, cvalue);
-
 	for (index = optind; index < argc; index++)
-		printf ("Non-option argument %s\n", argv[index]);
+    	ErrorFound(7);
+
+	if (hflag == true && strcmp(avalue, "") == 0 && strcmp(svalue, "") == 0 && strcmp(fvalue, "") == 0 && lvalue == -2)
+	{
+		PrintHelp();
+	}
+	else
+	{
+		ErrorFound(8);
+	}
+
+	printf ("hflag = %d, avalue = %s, svalue = %s, lvalue = %d, fvalue = %s\n", hflag, avalue, svalue, lvalue, fvalue);
+
 	return 0;
 }
