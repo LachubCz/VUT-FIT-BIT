bd97a43311273d90017d44f6ff9343c29a80d3bd
LachubCz <petr.genius.buchal@gmail.com>
2017-11-13 18:35:49 +0100

    first update of final week

--- 5_semestr/ISA/proj1/isashark.c
+++ 5_semestr/ISA/proj1/isashark.c
@@ -13,24 +13,38 @@
 #define PCAP_ERRBUF_SIZE (256)
 #endif
 
-struct sniff_ip {
-#if BYTE_ORDER == LITTLE_ENDIAN
-             u_char  ip_hl:4,                /* header length */
-                     ip_v:4;                 /* version */
-#endif
-#if BYTE_ORDER == BIG_ENDIAN
-            u_char  ip_v:4,                 /* version */
-                    ip_hl:4;                /* header length */
-#endif
+struct IPv6Header {
+    union {
+        struct ip6_hdrctl {
+            u_int32_t ip6_un1_flow;	/* 20 bits of flow-ID */
+            u_int16_t ip6_un1_plen;	/* payload length */
+            u_int8_t  ip6_un1_nxt;	/* next header */
+            u_int8_t  ip6_un1_hlim;	/* hop limit */
+        } ip6_un1;
+        u_int8_t ip6_un2_vfc;	/* 4 bits version, top 4 bits class */
+    } ip6_ctlun;
+    struct in6_addr ip6_src;	/* source address */
+    struct in6_addr ip6_dst;	/* destination address */
+};
+
+struct IPv4Header {
+	#if BYTE_ORDER == LITTLE_ENDIAN
+        u_char  ip_hl:4,                /* header length */
+        ip_v:4;                 /* version */
+	#endif
+	#if BYTE_ORDER == BIG_ENDIAN
+        u_char  ip_v:4,                 /* version */
+        ip_hl:4;                /* header length */
+	#endif
     u_char ip_tos;		    /* type of service */
     u_short ip_len;		    /* total length */
     u_short ip_id;		    /* identification */
     u_short ip_off;		    /* fragment offset field */
-#define	IP_RF 0x8000			/* reserved fragment flag */
-#define	IP_DF 0x4000			/* dont fragment flag */
-#define	IP_MF 0x2000			/* more fragments flag */
-#define	IP_OFFMASK 0x1fff		/* mask for fragmenting bits */
-/*(flags_fragmentOffset & 0x4000) != 0*/
+	#define	IP_RF 0x8000			/* reserved fragment flag */
+	#define	IP_DF 0x4000			/* dont fragment flag */
+	#define	IP_MF 0x2000			/* more fragments flag */
+	#define	IP_OFFMASK 0x1fff		/* mask for fragmenting bits */
+	/*(flags_fragmentOffset & 0x4000) != 0*/
     u_char ip_ttl;		    /* time to live */
     u_char ip_p;		    /* protocol */
     u_short ip_sum;		    /* checksum */
@@ -48,11 +62,6 @@ struct ADHeader {
 };
 
 struct QHeader {
-  /* u_char Q_dhost[6];
-    u_char Q_shost[6];
-    u_short Q_tpid;
-    u_short Q_ether_type;
- */
     uint8_t Q_dhost[6];
     uint8_t Q_shost[6];
     uint16_t Q_tpid1;
@@ -210,7 +219,9 @@ int main (int argc, char *argv[])
   	struct ether_header *eptr;
   	struct ADHeader *adptr;
 	struct QHeader *qptr;
-	struct sniff_ip *ip_layer;
+	struct IPv4Header *ipv4ptr;
+	struct IPv6Header *ipv6ptr;
+
 
 	if ((handle = pcap_open_offline(filename,errbuf)) == NULL)
 		ErrorFound(9);
@@ -237,12 +248,14 @@ int main (int argc, char *argv[])
     		EthTypeSize = 14;
     		printf("| Ethernet %s %s\n",ether_ntoa((const struct ether_addr *)&eptr->ether_shost), ether_ntoa((const struct ether_addr *)&eptr->ether_dhost));
     		//printf("ETHERTYPE_IP IPV4\n");
+    		ipv4ptr = (struct IPv4Header*) (packet+14);
     		break;
 
     		case 0x86DD: //ETHERTYPE_IP IPV6
     		EthTypeSize = 14;
     		printf("| Ethernet %s %s\n",ether_ntoa((const struct ether_addr *)&eptr->ether_shost), ether_ntoa((const struct ether_addr *)&eptr->ether_dhost));
     		//printf("ETHERTYPE_IP IPV6\n");
+    		ipv6ptr = (struct IPv6Header*) (packet+14);
     		break;
 
     		case 0x8100: //IEEE 802.1Q
@@ -259,11 +272,10 @@ int main (int argc, char *argv[])
     				if (ntohs(qptr->Q_ether_type) == 0x86DD)
     				{
     					printf("IPV6\n" );
-    					    				u_int size_ip;
+    					u_int size_ip;
 
-    				ip_layer = (struct sniff_ip*) (packet+EthTypeSize);
-    				//size_ip = ip->ip_hl;
-    				switch (ip_layer->ip_p){
+    				ipv6ptr = (struct IPv6Header*) (packet+18);
+    				switch (ipv6ptr->ip_p){
     					case 6:
     					printf("TCT\n");
     					break;
@@ -271,6 +283,7 @@ int main (int argc, char *argv[])
 						printf("UDP\n");
 						break;
 						default:
+						printf("%x\n", ipv6ptr->ip_p);
 						printf("shit\n");
 						break;
     			}
