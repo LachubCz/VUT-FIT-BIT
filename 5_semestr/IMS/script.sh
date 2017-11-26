44e5e05c9a4909a5a34baed67b224ae686398425
LachubCz <petr.genius.buchal@gmail.com>
2017-11-26 12:15:55 +0100

    script na averageyear

---
+++ 5_semestr/IMS/script.sh
@@ -0,0 +1,157 @@
+#!/bin/sh
+   echo "print"
+   output1="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "($output1 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r' | tr -d $'\r'
+   output2="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output2 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output3="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output3 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output4="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output4 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output5="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output5 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output6="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output6 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output7="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output7 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output8="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output8 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output9="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output9 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output10="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output10 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output11="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output11 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output12="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output12 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output13="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output13 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output14="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output14 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output15="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output15 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output16="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output16 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output17="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output17 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output18="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output18 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output19="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output19 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output20="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output20 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output21="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output21 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output22="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output22 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output23="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output23 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output24="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output24 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output25="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output25 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output26="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output26 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output27="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output27 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output28="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output28 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output29="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output29 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output30="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output30 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output31="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output31 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output32="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output32 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output33="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output33 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output34="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output34 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output35="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output35 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output36="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output36 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output37="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output37 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output38="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output38 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output39="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output39 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output40="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output40 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output41="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output41 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output42="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output42 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output43="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output43 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output44="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output44 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output45="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output45 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output46="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output46 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output47="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output46 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output48="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output48 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output49="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output49 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output50="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output50 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output51="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output51 + " | tr -d $'\r'
+   sleep 1s | tr -d $'\r'
+   output52="$(make run | grep "DC3(2)" | wc -l | tr -d $'\r')"
+   echo -n "$output52) / 50" | tr -d $'\r'
