823cdebd123ccf7bfa074c4394c4b5cb68a65b91
LachubCz <petr.genius.buchal@gmail.com>
2018-08-02 15:55:24 +0200

    changes due to change to public repository

--- 6_semestr/ISZ/timer.py
+++
@@ -1,9 +0,0 @@
-import datetime
-import time
-
-then = datetime.datetime(2018, 6, 11, 8, 0, 0, 0)
-
-while True:
-    time.sleep(1)
-    now = datetime.datetime.now()
-    print("#######", ((then - now)/38), "#######", end='\r')
